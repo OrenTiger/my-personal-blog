4dbf9ebd63c912ac4d44beae0d3ae0212236ed2a
Timur Basyrov <timmthetiger@mail.ru>
2015-11-18 19:39:10 +0500

    Modified db initializer

--- MyPersonalBlog/Repositories/BlogDbInitializer.cs
+++ MyPersonalBlog/Repositories/BlogDbInitializer.cs
@@ -14,9 +14,15 @@ namespace MyPersonalBlog.Repositories
             List<Post> testPosts = new List<Post>();
 
             // TODO: Добавить больше записей
-            testPosts.Add(new Post { Id = 1, Title = "Test blog  post #1", IntroText = "Intro Text", MainText = "Main Text", CreateDate = DateTime.Now, Published = true });
+            testPosts.Add(new Post { Id = 1, Title = "Test blog post #1", IntroText = "Intro Text", MainText = "Main Text", CreateDate = DateTime.Now, Published = true });
             testPosts.Add(new Post { Id = 2, Title = "Test blog post #2", IntroText = "Intro Text", MainText = "Main Text", CreateDate = DateTime.Now, Published = true });
             testPosts.Add(new Post { Id = 3, Title = "Test blog post #3", IntroText = "Intro Text", MainText = "Main Text", CreateDate = DateTime.Now, Published = true });
+            testPosts.Add(new Post { Id = 4, Title = "Test blog post #4", IntroText = "Intro Text", MainText = "Main Text", CreateDate = DateTime.Now, Published = false });
+            testPosts.Add(new Post { Id = 5, Title = "Test blog post #5", IntroText = "Intro Text", MainText = "Main Text", CreateDate = DateTime.Now, Published = true });
+            testPosts.Add(new Post { Id = 6, Title = "Test blog post #6", IntroText = "Intro Text", MainText = "Main Text", CreateDate = DateTime.Now, Published = true });
+            testPosts.Add(new Post { Id = 7, Title = "Test blog post #7", IntroText = "Intro Text", MainText = "Main Text", CreateDate = DateTime.Now, Published = false });
+            testPosts.Add(new Post { Id = 8, Title = "Test blog post #8", IntroText = "Intro Text", MainText = "Main Text", CreateDate = DateTime.Now, Published = true });
+            testPosts.Add(new Post { Id = 9, Title = "Test blog post #9", IntroText = "Intro Text", MainText = "Main Text", CreateDate = DateTime.Now, Published = true });
 
             testPosts[0].IntroText = "Lorem ipsum dolor sit amet, id scaevola maluisset omittantur vis, vivendum praesent assentior his id. In vis commodo conclusionemque. Aliquip docendi antiopam his eu. In illud patrioque similique sed. At mea nostrum percipitur, quidam quaeque petentium nam ea. Eum cu tractatos patrioque dissentias, at mel consul vocent, nibh duis legendos eam at.";
             testPosts[0].MainText = "Ad nam commodo numquam verterem. Vis ea illum zril, quo no adhuc everti periculis. Delenit definitiones has ne, nec eu maluisset rationibus. Cu liber fierent ius. Eu mei animal molestie splendide, te mei suas wisi elitr. At iriure quaeque alienum vix. Ius ut eligendi comprehensam, veniam debitis signiferumque ex vim. Laudem aperiri ex mei, doming mediocrem pri ex. Omnium sapientem vim an, nostro admodum percipitur eu sed. Ex partem graecis ius. Novum pertinacia cotidieque in eam, paulo nemore tincidunt vis ut. Has cu nulla nominati, admodum eligendi appellantur et vix, posse graece ad duo. Eum an oblique persius vivendum, summo salutatus ad est. Vis et saperet debitis. Cu epicuri volutpat cotidieque eos. Mei case laoreet no, phaedrum sapientem inciderint ad ius. Everti lucilius quaestio ei nam, sonet choro ne per, natum animal fuisset duo ea. Te verterem reprimique interesset sit, everti sanctus minimum mea at. Id justo postulant est, te nam essent gloriatur, duo volumus detraxit te. In labore delectus per, in per graece denique. Et mei sale omnis, aliquid copiosae laboramus ei vel. Mei ne ludus munere impedit, modus nonumes ne vim.";
@@ -24,6 +30,12 @@ namespace MyPersonalBlog.Repositories
             db.Posts.Add(testPosts[0]);
             db.Posts.Add(testPosts[1]);
             db.Posts.Add(testPosts[2]);
+            db.Posts.Add(testPosts[3]);
+            db.Posts.Add(testPosts[4]);
+            db.Posts.Add(testPosts[5]);
+            db.Posts.Add(testPosts[6]);
+            db.Posts.Add(testPosts[7]);
+            db.Posts.Add(testPosts[8]);
 
             List<Tag> testTags = new List<Tag>();
 
